b9fe62b36b9478ed00f0ca5626243ca76a7cc29e
ljh <1302450598@qq.com>
2018-11-20 20:25:03 +0800

    day03-前端分层开发【MVC分层】

---
+++ pinyougou-web/pinyougou-manager-web/src/main/webapp/js/base-pagination.js
@@ -0,0 +1,2 @@
+/* 定义模块,带分页,固定格式 :定义模块语法格式：var 变量名 = angular.module(“模块名”, []) */
+var app = angular.module("pinyougou",['pagination']);
---
+++ pinyougou-web/pinyougou-manager-web/src/main/webapp/js/base.js
@@ -0,0 +1,2 @@
+/* 定义基础模块,不带分页模块,固定格式 :定义模块语法格式：var 变量名 = angular.module(“模块名”, []) */
+var app = angular.module("pinyougou",[]);
